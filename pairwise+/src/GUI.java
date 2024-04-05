260164335ad47496d19a5529920830ee51be2c33
hello-5555 <166086860+hello-5555@users.noreply.github.com>
2024-04-04 23:54:32 -0500

    Add files via upload
    made gui show a label and tried to show a text box

--- pairwise+/src/GUI.java
+++ pairwise+/src/GUI.java
@@ -1,14 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
-
+import javax.swing.border.Border;
 class gui {
     public static void main(String[] args){
+        //Border border = BorderFactory.createLineBorder(Color.black,1);
+        //creates a label and adds text
+        JLabel label = new JLabel();
+        label.setText("Team #:");
+        //label.setBorder(border);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
+        //location of the text
+        label.setBounds(10,0,60,20);
+
+        JTextField team1 = new JTextField("Team #",6);
+
         JFrame frame = new JFrame();
         frame.setTitle("PAIRWISE+");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420,420);
+        frame.setSize(500,500);
         
+        frame.setLayout(null);
+        //makes frme show uo becuase the defult is dumb
         frame.setVisible(true);
+        frame.add(label);
+        frame.add(team1);
         frame.getContentPane().setBackground(new Color(5, 10, 70));
     }
 }
